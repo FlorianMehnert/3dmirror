68d4ff9bde05145817700c3cc096aa86a19338b3
Florian Mehnert <florian.mehnert@gmail.com>
2023-12-05 23:13:19 +0100

    include surfel renderer

--- project/mirror3D/mirror3D.cxx
+++ project/mirror3D/mirror3D.cxx
@@ -13,10 +13,18 @@
 #include <cgv/utils/pointer_test.h>
 #include <chrono>
 
+// specifically for mirror3D plugin
 #include <mirror3D.h>
-#include "frame.h"
+#include <frame.h>
+
+// constructing pointclouds
+#include "point_cloud/point_cloud.h"
+
+using namespace cgv::render;
 
 //#include <cgv_gl/point_renderer.h>
+#include <cgv_gl/surfel_renderer.h>
+
 //#include <cgv_gl/gl/gl.h>
 
 class mirror3D :
@@ -27,38 +35,62 @@ class mirror3D :
 	bool calib_outofdate = true;
 protected:
 	bool debug_frame_timing = false;
-
+	
 	// render data
 	std::vector<usvec3> sP;
 	std::vector<rgb8> sC;
 
-	// point cloud data
-	std::vector<vec3> P;
-	std::vector<rgb8> C;
 
-	// color and depth frames from kinect
-	rgbd::frame_type depth_frame;
-	rgbd::frame_type color_frame;
+	// using surfel renderer to use instanced rendering
+	cgv::render::surfel_render_style source_srs;
 
 	// color warped to depth image is only needed in case CPU is used to lookup colors
 	rgbd::frame_type warped_color_frame;
 
 	// rendering configuration
-	rgbd::rgbd_point_renderer pr;
 	cgv::render::point_render_style prs;
 	cgv::render::texture depth_tex, color_tex;
 
 	cgv::render::view* view_ptr = 0;
 
+	// point cloud data
+	std::vector<vec3> P, P2;
+	std::vector<rgb8> C, C2;
+
+	// init empty pointcloud of type pointcloud
+	point_cloud &pcl = point_cloud();
+
+	// color and depth frames from kinect
+	rgbd::frame_type depth_frame;
+	rgbd::frame_type color_frame;
+
+	// shader
+	cgv::render::shader_program rgbd_prog;
+
+	// texture, shaders and display lists
+	cgv::render::texture color;
+
+	// default point renderer
+	rgbd::rgbd_point_renderer pr;
+
 	// toggle via gui - construct pcl or use frames and render points
-	bool render_pcl = true;
+	bool surfel = true;
 public:
 	mirror3D() : color_tex("uint8[R,G,B]"), depth_tex("uint16[R]")
-	{
+	{	
 		set_name("mirror3D");
 		pr.set_distortion_map_usage(true);
+		pr.set_geometry_less_rendering(false);
 		prs.point_size = 5;
 		prs.blend_width_in_pixel = 0;
+		
+		// set surfel style
+		source_srs.measure_point_size_in_pixel = false;
+		source_srs.point_size = 0.25f;
+		source_srs.blend_width_in_pixel = 1.0f;
+		source_srs.blend_points = true;
+		source_srs.illumination_mode = cgv::render::IM_TWO_SIDED;
+
 	}
 	void on_set(void* member_ptr)
 	{
@@ -103,6 +135,7 @@ public:
 			std::cout << " -> " << (depth_frame.device_time_stamp - color_frame.device_time_stamp) * 1e-6;
 			std::cout << std::endl;
 		}
+		
 		post_redraw();
 	}
 	void on_stop() {
@@ -129,10 +162,12 @@ public:
 		}
 		return false;
 	}
+
 	void create_gui()
 	{
 		add_decorator("mirror3D", "heading", "level=1");
 		add_member_control(this, "debug_frame_timing", debug_frame_timing, "check");
+		add_member_control(this, "test_member", surfel, "check");
 		if (begin_tree_node("capture", is_running)) {
 			align("\a");
 			create_gui_base(this, *this);
@@ -146,17 +181,29 @@ public:
 			align("\a");
 			end_tree_node(prs);
 		}
+
+		// surfel_render
+		if (begin_tree_node("Surfel Rendering", source_srs)) {
+			align("\a");
+			//sr.create_gui(this, *this);
+			add_gui("surfel_style", source_srs);
+			align("\b");
+			end_tree_node(source_srs);
+		}
+		
 	}
 	bool init(cgv::render::context& ctx)
 	{
 		start_first_device();
 		ctx.set_bg_clr_idx(4);
 		cgv::render::ref_point_renderer(ctx, 1);
+		cgv::render::ref_surfel_renderer(ctx, 1);
 		return pr.init(ctx);
 	}
 	void clear(cgv::render::context& ctx)
 	{
 		cgv::render::ref_point_renderer(ctx, -1);
+		cgv::render::ref_surfel_renderer(ctx, -1);
 		pr.clear(ctx);
 		if (is_running) {
 			is_running = false;
@@ -165,234 +212,98 @@ public:
 	}
 	void init_frame(cgv::render::context& ctx)
 	{
-		if (!view_ptr)
-			view_ptr = find_view_as_node();
-		if (color_frame_changed) {
-			create_or_update_texture_from_frame(ctx, color_tex, color_frame);
-			color_frame_changed = false;
-		}
-		if (depth_frame_changed) {
-			create_or_update_texture_from_frame(ctx, depth_tex, depth_frame);
-			depth_frame_changed = false;
-		}
-		if (!pr.do_geometry_less_rendering()) {
-			if (!pr.do_lookup_color()) {
-				rgbd_inp.map_color_to_depth(depth_frame, color_frame, warped_color_frame);
-				rgbd::construct_rgbd_render_data_with_color(depth_frame, warped_color_frame, sP, sC);
+			if (!view_ptr)
+				view_ptr = find_view_as_node();
+			if (color_frame_changed) {
+				create_or_update_texture_from_frame(ctx, color_tex, color_frame);
+				color_frame_changed = false;
 			}
-			else
-				rgbd::construct_rgbd_render_data(depth_frame, sP);
-		}
+			if (depth_frame_changed) {
+				create_or_update_texture_from_frame(ctx, depth_tex, depth_frame);
+				
+				// adding surfel renderer on init frame (in draw_point_cloud in icp_tools)
+				cgv::render::surface_renderer& sr = ref_surfel_renderer(ctx);
+				sr.set_render_style(source_srs);
+				
+				depth_frame_changed = false;
+			}
+
+			// at this point sP and sC are populated with point data
+			if (!pr.do_geometry_less_rendering()) {
+				if (!pr.do_lookup_color()) {
+					rgbd_inp.map_color_to_depth(depth_frame, color_frame, warped_color_frame);
+					rgbd::construct_rgbd_render_data_with_color(depth_frame, warped_color_frame, sP, sC);
+				}
+				else
+					rgbd::construct_rgbd_render_data(depth_frame, sP);	
+			}
+
+			// populate pointcloud
+			if (surfel) {
+				pcl.clear();
+				for (cgv::render::uvec3 point : sP) {
+					pcl.add_point(point_cloud_types::Pnt(point));
+				}
+			}
 	}
+
 	void draw(cgv::render::context& ctx)
 	{
-		render_pcl = true;
-		if (!render_pcl) {
-			if (!pr.ref_prog().is_linked())
-				return;
-			if (!depth_tex.is_created())
-				return;
+		if (!pr.ref_prog().is_linked())
+			return;
+		if (!depth_tex.is_created())
+			return;
 
-			glDisable(GL_CULL_FACE);
-			if (calib_outofdate) {
-				pr.set_calibration(calib);
-				calib_outofdate = false;
-			}
-			if (view_ptr)
-				pr.set_y_view_angle(float(view_ptr->get_y_view_angle()));
-			pr.set_render_style(prs);
-			if (!pr.do_geometry_less_rendering())
-				pr.set_position_array(ctx, sP);
-			if (!pr.do_lookup_color())
-				pr.set_color_array(ctx, sC);
-			if (pr.validate_and_enable(ctx)) {
-				depth_tex.enable(ctx, 0);
-				if (pr.do_lookup_color())
-					color_tex.enable(ctx, 1);
-				if (pr.do_geometry_less_rendering())
-					pr.ref_prog().set_uniform(ctx, "depth_image", 0);
-				pr.ref_prog().set_uniform(ctx, "color_image", 1);
+		glDisable(GL_CULL_FACE);
+		if (calib_outofdate) {
+			pr.set_calibration(calib);
+			calib_outofdate = false;
+		}
+		if (view_ptr)
+			pr.set_y_view_angle(float(view_ptr->get_y_view_angle()));
+		pr.set_render_style(prs);
+		if (!pr.do_geometry_less_rendering())
+			pr.set_position_array(ctx, sP);
+		if (!pr.do_lookup_color())
+			pr.set_color_array(ctx, sC);
+		if (pr.validate_and_enable(ctx)) {
+			depth_tex.enable(ctx, 0);
+			if (pr.do_lookup_color())
+				color_tex.enable(ctx, 1);
+			if (pr.do_geometry_less_rendering())
+				pr.ref_prog().set_uniform(ctx, "depth_image", 0);
+			pr.ref_prog().set_uniform(ctx, "color_image", 1);
+			if (!surfel) {
 				pr.draw(ctx, 0, sP.size());
-				pr.disable(ctx);
-				if (pr.do_lookup_color())
-					color_tex.disable(ctx);
-				if (pr.do_geometry_less_rendering())
-					depth_tex.disable(ctx);
 			}
-			glEnable(GL_CULL_FACE);
+			pr.disable(ctx);
+			if (pr.do_lookup_color())
+				color_tex.disable(ctx);
+			if (pr.do_geometry_less_rendering())
+				depth_tex.disable(ctx);
 		}
-		else{
-			// very slow point_cloud drawing
-			construct_point_cloud();
+		
+		// draw surfels
+		cgv::render::surfel_renderer& sr = ref_surfel_renderer(ctx);
+		sr.set_render_style(source_srs);
+		if (surfel) {
+			ctx.push_modelview_matrix();
+			if (pcl.get_nr_points() > 0) {
+				int num_points = pcl.get_nr_points();
+				sr = ref_surfel_renderer(ctx);
+				sr.set_position_array(ctx, &pcl.pnt(0), num_points);
+				// would require to calculate normals first
+				//sr.set_normal_array(ctx, &pcl.nml(0), num_points);
 
-			// enable shader using the code from rgbd_control
-			if (M_POINTS.size() > 0) {
-				ctx.mul_modelview_matrix(cgv::math::translate4<double>(1.5, 0, 0));
-				if (M_TRIANGLES.size() > 0) {
-					ctx.mul_modelview_matrix(cgv::math::scale4<double>(1.0 / 128, 1.0 / 128, -1.0 / 128));
-					cgv::render::shader_program& prog = rgbd_prog;
-					if (prog.is_created()) {
-						color.enable(ctx, 0);
-						prog.set_uniform(ctx, "color_texture", 0);
-						prog.enable(ctx);
-						glDisable(GL_CULL_FACE);
-						float* tex_coords = nullptr;
-						if (M_UV.size() > 0) {
-							tex_coords = reinterpret_cast<float*>(M_UV.data());
-						}
-						ctx.draw_faces(reinterpret_cast<float*>(M_POINTS.data()), nullptr, tex_coords,
-							reinterpret_cast<int32_t*>(M_TRIANGLES.data()), nullptr, reinterpret_cast<int32_t*>(M_TRIANGLES.data()), int(M_TRIANGLES.size()), 3);
-						glEnable(GL_CULL_FACE);
-						prog.disable(ctx);
-						color.disable(ctx);
-					}
-					ctx.mul_modelview_matrix(cgv::math::scale4<double>(128, 128, -128));
-				}
-				else {
-					pr.set_render_style(prs);
-					pr.set_position_array(ctx, M_POINTS);
-					std::vector<rgba8> colors(M_POINTS.size(), rgba8(127, 127, 127, 255));
-					pr.set_color_array(ctx, colors);
-					if (pr.validate_and_enable(ctx)) {
-						glDrawArrays(GL_POINTS, 0, (GLsizei)M_POINTS.size());
-						pr.disable(ctx);
-					}
-				}
-				ctx.mul_modelview_matrix(cgv::math::translate4<double>(-1.5, 0, 0));
+				std::vector<cgv::render::vec4> point_color{ 0.0, 0.0, 1.0, 0.8 };
+				std::vector<cgv::math::fvec<float, 4>> color(num_points, vec4(0.0, 0.0, 1.0, 0.8));
+				sr.set_color_array(ctx, point_color);
+				sr.render(ctx, 0, num_points);
 			}
-
+			ctx.pop_modelview_matrix();
 		}
+		glEnable(GL_CULL_FACE);
 	}
-
-	// see rgbd_control for further references
-	size_t construct_point_cloud_cgv()
-	{
-		cgv::math::distortion_inversion_epsilon<double>();
-		for (uint16_t y = 0; y < calib.depth.h; ++y) {
-			for (uint16_t x = 0; x < calib.depth.w; ++x) {
-				uint16_t depth = reinterpret_cast<const uint16_t&>(depth_frame.frame_data[(y * calib.depth.w + x) * depth_frame.get_nr_bytes_per_pixel()]);
-				if (depth == 0)
-					continue;
-				uint8_t* pix_ptr = reinterpret_cast<uint8_t*>(&warped_color_frame.frame_data[(y * calib.depth.w + x) * warped_color_frame.get_nr_bytes_per_pixel()]);
-				rgba8 c(pix_ptr[2], pix_ptr[1], pix_ptr[0], 255);
-
-				vec3 p;
-				vec2 xd;
-
-				// if used distortion map
-				if (false) {
-					continue;
-				}
-				else {
-					unsigned iterations = 1;
-					dvec2 xu = calib.depth.pixel_to_image_coordinates(dvec2(double(x), double(y)));
-					dvec2 xd_T = xu;
-					auto dir = calib.depth.invert_distortion_model(xu, xd_T, true);
-					if (dir != cgv::math::distorted_pinhole_types::distortion_inversion_result::convergence)
-						continue;
-					xd = xd_T;
-				}
-				{
-					dvec3 p_d(depth * xd[0], depth * xd[1], depth);
-					p_d = 0.001 * ((p_d + pose_position(calib.color.pose)) * pose_orientation(calib.color.pose));
-					dvec2 xu, xd(p_d[0] / p_d[2], p_d[1] / p_d[2]);
-					auto result = calib.color.apply_distortion_model(xd, xu);
-					if (result != cgv::math::distorted_pinhole_types::distortion_result::success)
-						c = rgba8(128, 0, 0, 255);
-					else {
-						dvec2 xp = calib.color.image_to_pixel_coordinates(xu);
-						if (xp[0] < 0 || xp[1] < 0 || xp[0] >= color_frame.width || xp[1] >= color_frame.height)
-							c = rgba8(255, 0, 255, 255);
-						else {
-							uint16_t x = uint16_t(xp[0]);
-							uint16_t y = uint16_t(xp[1]);
-							const uint8_t* pix_ptr = reinterpret_cast<const uint8_t*>(&color_frame.frame_data[(y * color_frame.width + x) * color_frame.get_nr_bytes_per_pixel()]);
-							c = rgba8(pix_ptr[2], pix_ptr[1], pix_ptr[0], 255);
-						}
-					}
-				}
-				float d_m = 0.001f * depth;
-				p = vec3(-d_m * float(xd[0]), -d_m * float(xd[1]), d_m);
-				P.push_back(p);
-				C.push_back(c);
-			}
-		}
-		return P.size();
-	}
-
-	// see rgbd_control
-	size_t construct_point_cloud()
-	{
-		P.clear();
-		C.clear();
-		const unsigned short* depths = reinterpret_cast<const unsigned short*>(&depth_frame.frame_data.front());
-		const unsigned char* colors = reinterpret_cast<const unsigned char*>(&color_frame.frame_data.front());
-		// if remap_color
-		if (false) {
-			rgbd_inp.map_color_to_depth(depth_frame, color_frame, warped_color_frame);
-			// if do_bilateral_filter
-			if (false) {
-				//bilatral_filter();
-			}
-			colors = reinterpret_cast<const unsigned char*>(&warped_color_frame.frame_data.front());
-		}
-		// if cgv_reconstruct
-		if (false)
-			return construct_point_cloud_cgv();
-		unsigned bytes_per_pixel = color_frame.nr_bits_per_pixel / 8;
-		int i = 0;
-		float s = 1.0f / 255;
-		for (int y = 0; y < depth_frame.height; ++y)
-			for (int x = 0; x < depth_frame.width; ++x) {
-				vec3 p;
-				rgba8 point_color;
-				if (rgbd_inp.map_depth_to_point(x, y, depths[i], &p[0])) {
-					switch (color_frame.pixel_format) {
-					case rgbd::PF_BGR:
-					case rgbd::PF_BGRA:
-						if (color_frame.nr_bits_per_pixel == 32) {
-							point_color = rgba8(colors[bytes_per_pixel * i + 2], colors[bytes_per_pixel * i + 1], colors[bytes_per_pixel * i], colors[bytes_per_pixel * i + 3]);
-						}
-						else {
-							point_color = rgba8(colors[bytes_per_pixel * i + 2], colors[bytes_per_pixel * i + 1], colors[bytes_per_pixel * i], 255);
-						}
-						break;
-					case rgbd::PF_RGB:
-					case rgbd::PF_RGBA:
-						if (color_frame.nr_bits_per_pixel == 32) {
-							point_color = rgba8(colors[bytes_per_pixel * i], colors[bytes_per_pixel * i + 1], colors[bytes_per_pixel * i + 2], colors[bytes_per_pixel * i + 3]);
-						}
-						else {
-							point_color = rgba8(colors[bytes_per_pixel * i], colors[bytes_per_pixel * i + 1], colors[bytes_per_pixel * i + 2], 255);
-						}
-						break;
-					case rgbd::PF_BAYER:
-						point_color = rgba8(colors[i], colors[i], colors[i], 255);
-						break;
-					}
-					//filter points without color for 32 bit formats
-					static const rgba8 filter_color = rgba8(0, 0, 0, 0);
-					if (!(point_color == filter_color)) {
-						C.push_back(point_color);
-						//C2.push_back(gray_scale);
-						// flipping y to make it the same direction as in pixel y coordinate
-						p[1] = -p[1];
-						P.push_back(p);
-					}
-				}
-				++i;
-			}
-		//std::cout << "warpped_color_size: " << i << std::endl;
-		/* debug code to print out bounding box of points */
-		/*
-		box3 box;
-		for (const auto& p : P2)
-			box.add_point(p);
-		std::cout << "constructed " << P2.size() << " points with box = " << box << std::endl;
-		*/
-		return P.size();
-	}
-
 };
 
 #include <cgv/base/register.h>
--- project/mirror3D/mirror3D.h
+++ project/mirror3D/mirror3D.h
@@ -5,9 +5,3 @@
 std::vector<cgv::render::render_types::ivec3> M_TRIANGLES;
 std::vector<cgv::render::render_types::vec3> M_POINTS;
 std::vector<cgv::render::render_types::vec2> M_UV;
-
-// shader
-cgv::render::shader_program rgbd_prog;
-
-// texture, shaders and display lists
-cgv::render::texture color;
